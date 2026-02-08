dd87ae8d1d81c13272a6575779a26359470d975d
END5876 <rangerskevinleezongyan0227@gmail.com>
2026-02-08 13:54:10 +0800

    fix bilibili header

--- handlers/bilibiliHandler.js
+++ handlers/bilibiliHandler.js
@@ -26,7 +26,16 @@ const ytdlpPath = 'yt-dlp';
 // Headers
 const BILIBILI_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'Referer': 'https://www.bilibili.com/'
+    'Referer': 'https://www.bilibili.com/',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Cache-Control': 'max-age=0'
 };
 
 async function checkYtDlp() {
@@ -61,6 +70,7 @@ async function checkFFmpeg() {
         return false;
     }
 }
+
 
 function setupBilibiliCommands(client) {
     Promise.all([checkYtDlp(), checkFFmpeg()]).then(([ytdlp, ffmpeg]) => {
